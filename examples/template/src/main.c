f3980cc9fa320de89691f5408048d714e6c86484
Luna Catkins <lunarlambda@gmail.com>
2022-12-17 18:41:28 +0100

    cleanup from main

---
+++ examples/template/src/main.c
@@ -0,0 +1,12 @@
+#include "definitions.h"
+
+int main(void)
+{
+    REG_DISPCNT = VIDEO_MODE_3 | VIDEO_BG2_ENABLE;
+
+    MODE3_SCREEN[80][120] = COLOR_RED;
+    MODE3_SCREEN[80][136] = COLOR_GREEN;
+    MODE3_SCREEN[96][120] = COLOR_BLUE;
+
+    while (true) {}
+}
